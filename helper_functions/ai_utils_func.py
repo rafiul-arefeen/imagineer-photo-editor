d16597ba9140a78f2c8211e7e73a2afeaf8638bf
wasifuddin <79798771+wasifuddin@users.noreply.github.com>
2024-06-06 11:26:54 +0600

    img url input fixed

--- helper_functions/ai_utils_func.py
+++ helper_functions/ai_utils_func.py
@@ -1,18 +1,36 @@
 from openai import OpenAI
-from dotenv import load_dotenv
+import cv2
+import numpy as np
+import requests
+import os
 
-load_dotenv()
+llm_client = OpenAI(api_key="api-key")
 
-llm_client = OpenAI()
+def load_image_from_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        image_array = np.asarray(bytearray(response.content), dtype="uint8")
+        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        return image
+    else:
+        print("Error: Unable to download image")
+        return None
 
-# Creating variation of the input image
+def save_image(image, path):
+    image = cv2.resize(image,(512,512))
+    cv2.imwrite(path, image)
+
 def create_image_variation(image_path, n=1, size="512x512"):
+    image = load_image_from_url(image_path)
+    image_path = 'downloaded_image.png'
+    save_image(image, image_path)
 
-    response = llm_client.images.create_variation(
-        image=open(image_path, "rb"),
-        n=n,
-        size=size
-    )
+    with open(image_path, 'rb') as image_file:
+        response = llm_client.images.create_variation(
+            image=image_file,
+            n=n,
+            size=size
+        )
     return response.data[0].url
 
 def generate_image_from_text(prompt, model="dall-e-3", n=1, size="1024x1024"):
@@ -22,7 +40,6 @@ def generate_image_from_text(prompt, model="dall-e-3", n=1, size="1024x1024"):
         n=n,
         size=size
     )
+    print(response)
     return response.data[0].url
 
-
-print(generate_image_from_text("Generate a cat image"))
